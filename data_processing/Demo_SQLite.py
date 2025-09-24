11b5db02d945fecd94b73dbe9dd37be1ce30a622
unknown <25730005@ms.uit.edu.vn>
2025-09-24 09:16:25 +0700

    Added SQLite demo pg 50 slide 04 MLBA Data processing

--- data_processing/Demo_SQLite.py
+++ data_processing/Demo_SQLite.py
@@ -10,11 +10,34 @@ try:
     query = "SELECT * FROM InvoiceLine LIMIT 5;"
     cursor.execute(query)
     df=pd.DataFrame(cursor.fetchall())
+    # cursor.close()
+    print(df.head())
+
+# Exercise Slide 49/50
+    print("*"*40)
+    n = int(input("Danh sách Customer có tham gia >= N invoice, N="))
+    query = f"""
+        SELECT
+            C.CustomerID, COUNT(C.CustomerID)
+        FROM
+            Customer AS C
+        JOIN
+            Invoice AS I ON I.CustomerID = C.CustomerID
+        GROUP BY
+            C.CustomerID
+        HAVING
+            COUNT(C.CustomerID) >= {n};
+    """
+
+    cursor.execute(query)
+    df=pd.DataFrame(cursor.fetchall())
     cursor.close()
-    print(df)
+    print(df.head())
 
 except sqlite3.Error as error:
     print("Error while connecting to sqlite: ", error)
+except Exception as error:
+    print("Unexpected error:", error)
 finally:
     if sqliteConnection:
         sqliteConnection.close()
